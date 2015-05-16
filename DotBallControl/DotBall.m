ce5960f958ea7b8a57717add719af6662a46c09d
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-05-15 23:51:48 -0700

    added infrastructure for transformation

--- DotBallControl/DotBall.m
+++ DotBallControl/DotBall.m
@@ -21,11 +21,11 @@
     
     if (self) {
         dotRadius = 20;
-        NSLog(@"screen height: %f", [self getScreenHeight]);
+
         // place the dots
         
         // Step1: put all dots at the origin.
-        for (NSInteger i = 0; i < 10; i++) {
+        for (NSInteger i = 0; i < [self getNumOfDotsBasedOnDotRadius]; i++) {
             Dot* d = [[Dot layer]initWithFrame:CGRectMake([self getScreenWidth]/2 - dotRadius, [self getScreenHeight]/2 - dotRadius, dotRadius * 2, dotRadius * 2)];
             [self.layer addSublayer:d];
             [d setNeedsDisplay];
@@ -33,21 +33,32 @@
         
         // Step2: all dots transform to the top
         for (Dot* d in self.layer.sublayers) {
-            d.transform = CATransform3DMakeTranslation(0, BALL_RADIUS, 0); // not sure up is plus or minus
+            d.transform = CATransform3DMakeTranslation(0, 0, BALL_RADIUS); // not sure up is plus or minus
         }
         
-        // Step3: all dots transform evenly along x axis
+        // Step3: all dots rotate transform evenly along x, y axis
         float theta = 0;
-        for (Dot* d in self.layer.sublayers) {
+        float phi = 0;
+        float stepAngle = M_PI * 2 / self.layer.sublayers.count; // the step angle must be the same for theta and phi to make sure the dots are tiled evenly
+        for (NSInteger i = 0; i < self.layer.sublayers.count; i++) {
+            Dot* d = [self.layer.sublayers objectAtIndex:i];
             
             d.transform = CATransform3DConcat(d.transform, CATransform3DMakeRotation(theta, 1, 0, 0));
             
+            
+            phi += M_PI * 2 / self.layer.sublayers.count;
             theta += M_PI * 2 / self.layer.sublayers.count;
         }
         
     }
     
     return self;
+}
+
+- (NSInteger) getNumOfDotsBasedOnDotRadius
+{
+    //stub
+    return 20;
 }
 
 - (float)getScreenHeight
@@ -59,7 +70,7 @@
 {
     return [UIScreen mainScreen].bounds.size.width;
 }
-                      
+
 
 /*
 // Only override drawRect: if you perform custom drawing.
