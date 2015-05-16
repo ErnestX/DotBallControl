259ad6e015908664c854786eff7b1912291ed640
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-05-15 23:26:59 -0700

    tested transforms

--- DotBallControl/DotBall.m
+++ DotBallControl/DotBall.m
@@ -9,7 +9,7 @@
 #import "DotBall.h"
 #import "Dot.h"
 
-#define BALL_RADIUS 100.0
+#define BALL_RADIUS 200.0
 
 @implementation DotBall {
     float dotRadius;
@@ -23,8 +23,9 @@
         dotRadius = 20;
         NSLog(@"screen height: %f", [self getScreenHeight]);
         // place the dots
+        
         // Step1: put all dots at the origin.
-        for (NSInteger i = 0; i < 20; i++) {
+        for (NSInteger i = 0; i < 10; i++) {
             Dot* d = [[Dot layer]initWithFrame:CGRectMake([self getScreenWidth]/2 - dotRadius, [self getScreenHeight]/2 - dotRadius, dotRadius * 2, dotRadius * 2)];
             [self.layer addSublayer:d];
             [d setNeedsDisplay];
@@ -32,8 +33,18 @@
         
         // Step2: all dots transform to the top
         for (Dot* d in self.layer.sublayers) {
-            d.transform = CATransform3DMakeTranslation(0, 0, BALL_RADIUS); // not sure up is plus or minus
+            d.transform = CATransform3DMakeTranslation(0, BALL_RADIUS, 0); // not sure up is plus or minus
         }
+        
+        // Step3: all dots transform evenly along x axis
+        float theta = 0;
+        for (Dot* d in self.layer.sublayers) {
+            
+            d.transform = CATransform3DConcat(d.transform, CATransform3DMakeRotation(theta, 1, 0, 0));
+            
+            theta += M_PI * 2 / self.layer.sublayers.count;
+        }
+        
     }
     
     return self;
